331e8c929de2bedb54a7216721779fd88f805824
Pavel <osoko007@gmail.com>
2020-05-26 12:54:04 +0300

    Это комит от 26.05.2020

--- main.js
+++ main.js
@@ -2,68 +2,97 @@
 // Директива, которая ставится первой строкой в файле скрипта и обозначает, 
 // что используется новый стандарт EP6, старые ошибки тут не работают.
 
-var leftBord = 33;
-console.log (leftBord);
+let money = prompt("Ваш бюджет на месяц?");
 
-// alert("Hello world!");
+let time = prompt("Введите дату в формате YYYY-MM-DD");
 
-let person = {
-    name: "John",
-    age: 25,
-}
+let vopr = prompt("Введите обязательную статью расходов в этом месяце");
 
-console.log (person["name"]);
+let voprosik = prompt("Во сколько обойдется?");
 
-let answer = prompt("Вам есть 18?" , "");
-
-console.log(typeof(answer));
-
-let incr = 10,
-    decr = 10;
-
-// incr++;
-// decr--;
+let expenses = {
+    
+};
+console.log (expenses);
 
 
-// Инкримент увеличивает на 1
-// Префиксная форма(впереди переменной)-выводит значение после операции
-console.log(++incr);
+let appData = {
+    budget: money,
+    timeData: time,
+    expe: expenses,
+    optExp: "optionalExpenses",
+    incomeExp: "income",
+    saving: false
+};
+console.log (appData);
 
-// декримент уменьшает на 1
-// Постфиксная форма(в конце) выводит значение до изменения
-console.log(decr--);
-// Остаток от деления чисел
-console.log(5%2);
-
-console.log("2" == 2);
-
-// Один оператор "=" - присваивание;
-// Два оператора "==" - сравнение;
-// Три оператора "===" - Строгое сравнение по типу данных
+alert (money / 30);
+ 
 
 
-// Оператор && Обозначает значение "И" и является правдой только когда оба значения истина.
-let isChecked = true,
-    isClose = true;
 
-console.log(isChecked && isClose);
+// var leftBord = 33;
+// console.log (leftBord);
 
-// Оператор && Обозначает значение "И" и является правдой только когда оба значения истина.
-let isCheckedsa = true,
-    isClosesa = false;
+// // alert("Hello world!");
 
-console.log(isCheckedsa && isClosesa);
+// let person = {
+//     name: "John",
+//     age: 25,
+// }
 
-// Оператор "||" "ИЛИ" выводит true в случае если хотя бы одно значение правдиво.
+// console.log (person["name"]);
 
-let isPocket = true,
-    isChpocket = false;
+// let answer = prompt("Вам есть 18?" , "");
 
-console.log(isPocket || isChpocket);
+// console.log(typeof(answer));
 
-// Знак отрицание "!" переводит правду в лож и обратно.
-let isPockets = false,
-    isChpockets = false;
+// let incr = 10,
+//     decr = 10;
 
-console.log(isPockets || !isChpockets);
+// // incr++;
+// // decr--;
+
+
+// // Инкримент увеличивает на 1
+// // Префиксная форма(впереди переменной)-выводит значение после операции
+// console.log(++incr);
+
+// // декримент уменьшает на 1
+// // Постфиксная форма(в конце) выводит значение до изменения
+// console.log(decr--);
+// // Остаток от деления чисел
+// console.log(5%2);
+
+// console.log("2" == 2);
+
+// // Один оператор "=" - присваивание;
+// // Два оператора "==" - сравнение;
+// // Три оператора "===" - Строгое сравнение по типу данных
+
+
+// // Оператор && Обозначает значение "И" и является правдой только когда оба значения истина.
+// let isChecked = true,
+//     isClose = true;
+
+// console.log(isChecked && isClose);
+
+// // Оператор && Обозначает значение "И" и является правдой только когда оба значения истина.
+// let isCheckedsa = true,
+//     isClosesa = false;
+
+// console.log(isCheckedsa && isClosesa);
+
+// // Оператор "||" "ИЛИ" выводит true в случае если хотя бы одно значение правдиво.
+
+// let isPocket = true,
+//     isChpocket = false;
+
+// console.log(isPocket || isChpocket);
+
+// // Знак отрицание "!" переводит правду в лож и обратно.
+// let isPockets = false,
+//     isChpockets = false;
+
+// console.log(isPockets || !isChpockets);
 
